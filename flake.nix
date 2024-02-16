791dfffdd6f573eeeb5d32db7d6a5fbbfcd1784c
Johannes Gerold <git+commit@jgero.me>
2024-02-16 17:01:58 +0100

    feat(app): add scoreboard as sse demonstration

--- flake.nix
+++ flake.nix
@@ -3,66 +3,58 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
+    templ = {
+      url = "github:a-h/templ";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, rust-overlay, ... }:
-      let
-        overlays = [ (import rust-overlay) ];
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit overlays system; };
-        rustVersion = pkgs.rust-bin.stable.latest.default;
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = rustVersion;
-          rustc = rustVersion;
-        };
-        myRustBuild = rustPlatform.buildRustPackage {
-          pname =
-            "j-k-ratio-plus-L";
-          version = "0.1.0";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-        };
-        staticHtml = pkgs.buildNpmPackage {
-          name = "monaco-editor-frontend";
-          src = ./.;
-          npmDepsHash = "sha256-d5BSZ2yOQhOuSTkvOuWxiscvXHJnXjC+52tYxiyDl1Y=";
-          installPhase = ''
-            mkdir -p $out
-            mv build $out
-          '';
-        };
-        containerImage = pkgs.dockerTools.buildLayeredImage {
-          name = "ghcr.io/jgero/j-k-ratio-plus-uppercase-l";
-          tag = "latest";
-          contents = with pkgs; [
-            kotlin
-            coreutils
-            jd-cli
-          ];
-          maxLayers = 10;
-          config = { Cmd = [ "${myRustBuild}/bin/j-k-ratio-plus-L" "--production" "--static-path=${staticHtml}/build" ]; };
-        };
-      in
-      {
-        packages.${system} = {
-          rustPackage = myRustBuild;
-          container = containerImage;
-          frontend = staticHtml;
-        };
-        defaultPackage = myRustBuild;
-        devShell = pkgs.mkShell {
-          packages = with pkgs; [
-            cargo
-            kotlin
-            jd-cli
-            nodejs
-          ];
-          buildInputs =
-            [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
-        };
+  outputs = { self, nixpkgs, templ, treefmt-nix, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      templ-bin = templ.packages.${system}.templ;
+      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      # containerImage = pkgs.dockerTools.buildLayeredImage {
+      #   name = "ghcr.io/jgero/j-k-ratio-plus-uppercase-l";
+      #   tag = "latest";
+      #   contents = with pkgs; [
+      #     kotlin
+      #     coreutils
+      #     jd-cli
+      #   ];
+      #   maxLayers = 10;
+      #   # config = { Cmd = [ "${myRustBuild}/bin/j-k-ratio-plus-L" "--production" "--static-path=${staticHtml}/build" ]; };
+      # };
+    in
+    {
+      formatter.${system} = treefmtEval.config.build.wrapper;
+      checks.${system}.formatter = treefmtEval.config.build.check self;
+      packages.${system}.default = pkgs.buildGoModule {
+        pname = "j-k-ratio-plus-L";
+        version = "2.0";
+        # vendorHash = nixpkgs.lib.fakeHash;
+        vendorHash = "sha256-FeGap2zXQCIFG894mUOHMDVLR34B84qfXZEGAF4ayjw=";
+        src = ./.;
+        nativeBuildInputs = [ templ-bin ];
+        preBuild = ''
+          templ generate hello.templ
+        '';
       };
+      # defaultPackage = myRustBuild;
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          go
+          gopls
+          templ-bin
+          kotlin
+          jd-cli
+          nodejs
+        ];
+      };
+    };
 }
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	app := echo.New()
+	app.GET("/", HomeHandler)
+	app.GET("/scoreboard", ScoreboardHandler)
+	app.Logger.Fatal(app.Start(":4000"))
+}
+
+// This custom Render replaces Echo's echo.Context.Render() with templ's templ.Component.Render().
+func Render(ctx echo.Context, statusCode int, t templ.Component) error {
+	ctx.Response().Writer.WriteHeader(statusCode)
+	ctx.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTML)
+	return t.Render(ctx.Request().Context(), ctx.Response().Writer)
+}
+
+func ScoreboardHandler(c echo.Context) error {
+	c.Response().Header().Set(echo.HeaderContentType, "text/event-stream")
+	c.Response().Header().Set(echo.HeaderCacheControl, "no-cache")
+	c.Response().Header().Set("Connection", "keep-alive")
+	// c.Response().Header().Set("Transfer-Encoding", "chunked")
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(c.Response().Writer, "event: ScoreboardUpdate\ndata: <div>hello world #%d<div>\n\n", i)
+		c.Response().Flush()
+		time.Sleep(1 * time.Second)
+	}
+	return nil
+
+	// return Render(c, http.StatusOK, scoreboard(map[string]string {
+	// 	"john": "hello world",
+	// }))
+}
+
+func HomeHandler(c echo.Context) error {
+	return Render(c, http.StatusOK, doc())
+}
